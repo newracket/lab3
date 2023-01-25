17ff3dbe5dbe25d38716a00812c7a30fba0081d8
newracket <newracket@users.noreply.github.com>
2023-01-25 15:32:12 -0800

    Added test for averageWithoutLowest function

--- ArrayTests.java
+++ ArrayTests.java
@@ -29,4 +29,17 @@ public class ArrayTests {
         int[] output = ArrayExamples.reversed(input);
         assertArrayEquals(new int[]{5, 4, 3, 2, 1}, output);
     }
+
+    @Test
+    public void testAverageWithoutLowest() {
+        double[] input1 = {};
+        double[] input2 = {1, 1, 1, 1, 3};
+        double[] input3 = {1, 2, 3, 4, 7};
+        double[] input4 = {-5, -2, 0, 4, 6};
+
+        assertEquals(0.0, ArrayExamples.averageWithoutLowest((input1)), 0.1);
+        assertEquals(0.75, ArrayExamples.averageWithoutLowest((input2)), 0.1);
+        assertEquals(4.0, ArrayExamples.averageWithoutLowest((input3)), 0.1);
+        assertEquals(2.0, ArrayExamples.averageWithoutLowest((input4)), 0.1);
+    }
 }
